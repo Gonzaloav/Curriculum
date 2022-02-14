5c6e440ed485d279e13cee4a4fa71bd7300dbe47
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-02-14 08:50:12 +0100

    intex.js

--- index.js
+++ index.js
@@ -1,4 +1,26 @@
-function main ( ) {
-    console.log ("Starting...");
+function imprimeHola (){
+    console.log ("Hola")
 }
-window.addEventListener ("load", main); 
+
+window.addEventListener("load", main)
+
+function main (){
+    console.log ("Sarting");
+    const titulo = document.querySelector ("h1");
+    titulo.addEventListener ("pointerenter", imprimeHola);
+    const elements = Array.from (document.body.querySelectorAll ("*"));
+    console.log (elements);
+    const counters = elements.map(
+        (item)=>{
+        return{
+            element: item, 
+            Counter: 0,
+        } 
+        }
+    )
+}
+
+   
+
+
+
