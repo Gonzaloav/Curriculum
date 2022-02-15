128892ca4ee848c470f3a878f1ccb96efce4284d
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-02-15 11:16:12 +0100

    blog.html

---
+++ src/js/index.js
@@ -0,0 +1,27 @@
+function imprimeHola (){
+    console.log ("Hola")
+}
+
+window.addEventListener("load", main)
+
+function main (){
+    console.log ("Sarting");
+    const titulo = document.querySelector ("h1");
+    titulo.addEventListener ("pointerenter", imprimeHola);
+    const elements = Array.from (document.body.querySelectorAll ("*"));
+    console.log (elements);
+    const counters = elements.map(
+        (item)=>{
+        return{
+            element: item, 
+            Counter: 0,
+        } 
+        }
+    )console.log (conter)
+}
+
+  <a href=  "index.html">Inico</a> 
+  
+
+
+
